589985aa78d58517b66f8390910a319eec7cc328
Sumair Irshad <sumairirshad@gmail.com>
2024-10-14 04:06:18 -0700

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {ChakraProvider } from "@chakra-ui/react"
+import { ContextProvider } from "./Context";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ContextProvider>
+  </React.StrictMode>
+);
+
+
